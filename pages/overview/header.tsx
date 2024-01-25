45178be0dc2357a8decb498e723e013a6e694852
Justice Eziefule <justice@paystack.com>
2024-01-25 03:25:36 +0100

    finish header

--- pages/overview/header.tsx
+++ pages/overview/header.tsx
@@ -1,10 +1,27 @@
 import classNames from "classnames";
+import Image from "next/image";
+
 import styles from "@/app/page.module.css";
 
 export default function Header() {
   return (
     <section className={classNames(styles.section, styles.header_section)}>
-      <div className={styles.title_wrapper}></div>
+      <div className={styles.title_wrapper}>
+        <Image
+          src="/usdc.svg"
+          alt="stablecoin logo"
+          width={50}
+          height={50}
+          className={styles.stablecoin}
+        />
+        <h1 className={styles.title}>USDC Wallet</h1>
+      </div>
+
+      <div className={styles.balance_wrapper}>
+        <h5 className={styles.balance_title}>Available Balance</h5>
+
+        <h3 className={styles.balance}>23,890.62</h3>
+      </div>
     </section>
   );
 }
