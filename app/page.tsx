3e5cc3f86d1b92cfe36d321ba58f31eb49718791
Justice Eziefule <justice@paystack.com>
2024-01-25 02:09:39 +0100

    design section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import classNames from "classnames";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>Hello World</p>
+      <section
+        className={classNames(styles.section, styles.header_section)}
+      ></section>
     </main>
   );
 }
